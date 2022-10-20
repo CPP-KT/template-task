42af0278f3ef41b61a7fd5fd3fa7252dac34a3e0
Zakhar Koval <Zakhar.Koval@jetbrains.com>
2022-10-20 16:09:19 +0300

    Introduced triplet files and toolchain files

---
+++ ci-extra/clang-toolchain.cmake
@@ -0,0 +1,7 @@
+set(CMAKE_C_COMPILER /usr/bin/clang-15)
+set(CMAKE_CXX_COMPILER /usr/bin/clang-15)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+if (CMAKE_BUILD_TYPE MATCHES "Debug")
+  message(STATUS "Enabling _GLIBCXX_DEBUG...")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_GLIBCXX_DEBUG")
+endif()
---
+++ ci-extra/gnu-toolchain.cmake
@@ -0,0 +1,6 @@
+set(CMAKE_C_COMPILER /usr/bin/gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/g++)
+if (CMAKE_BUILD_TYPE MATCHES "Debug")
+  message(STATUS "Enabling _GLIBCXX_DEBUG...")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_GLIBCXX_DEBUG")
+endif()
