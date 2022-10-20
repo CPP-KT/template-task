fcba1f61afd8f04ad7d990e8fb71a0f8be4fd22c
Zakhar Koval <Zakhar.Koval@jetbrains.com>
2022-10-20 16:48:59 +0300

    Enabled lld for clang toolchain

--- ci-extra/clang-toolchain.cmake
+++ ci-extra/clang-toolchain.cmake
@@ -1,6 +1,6 @@
 set(CMAKE_C_COMPILER /usr/bin/clang-14)
 set(CMAKE_CXX_COMPILER /usr/bin/clang-14)
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++ -fuse-ld=lld")
 if (CMAKE_BUILD_TYPE MATCHES "Debug")
   message(STATUS "Enabling _GLIBCXX_DEBUG...")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_GLIBCXX_DEBUG")
