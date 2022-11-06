5bba620cd41c278cddd15dd1a9a886f38db9f4a6
Lev Dovzhik <levdovzhik@gmail.com>
2022-11-06 22:07:59 +0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -3,7 +3,7 @@
 
 static int sum(int a, int b) {
   // In normal tasks, never change files with tests but here you can :)
-  return a - b; // TODO: fix me
+  return a + b; // TODO: fix me
 }
 
 TEST(something, simple) {
