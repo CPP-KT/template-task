aac2bfc54138f7e4ec331cab739f44743492f83c
Artem Koton <kotonartem@protonmail.com>
2023-04-29 17:20:52 +0300

    Add separate directories for source files and tests

--- test/dummy_test.cpp
+++ test/dummy_test.cpp
@@ -6,6 +6,6 @@ static void throwing_func() {
   throw std::logic_error("some exception");
 }
 
-TEST(something, test_abi) {
+TEST(dummy_test, test_abi) {
   EXPECT_THROW(throwing_func(), std::logic_error);
 }
