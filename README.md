a74b244091b6f08c854de979d148c2004f3683e1
Artem Koton <kotonartem@protonmail.com>
2023-05-07 00:27:30 +0300

    Create a template file for source code

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 > NOTE: Change project name in CMakeLists.txt for another task
 
 > NOTE: Change ci-extra/set-upstream.sh for another task
+
+> NOTE: Rename src/solution.h according to task
--- test/dummy_test.cpp
+++ test/dummy_test.cpp
@@ -1,10 +1,8 @@
+#include "solution.h"
+
 #include <gtest/gtest.h>
 
 #include <stdexcept>
-
-static void throwing_func() {
-  throw std::logic_error("some exception");
-}
 
 TEST(dummy_test, test_abi) {
   EXPECT_THROW(throwing_func(), std::logic_error);
