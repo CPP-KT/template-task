fed2676f9e88b7835ceac8a2c06e54ee059f1461
Artem Koton <kotonartem@protonmail.com>
2025-03-17 16:56:26 +0300

    Overhaul CMake toolchains

--- ci-extra/build.sh
+++ ci-extra/build.sh
@@ -6,7 +6,7 @@ BUILD_TYPE=$1
 # Configure CMake
 cmake -S . -B cmake-build-"$BUILD_TYPE" \
   --preset "$BUILD_TYPE" -G Ninja \
-  -DTREAT_WARNINGS_AS_ERRORS=ON
+  -D CT_TREAT_WARNINGS_AS_ERRORS=ON
 
 # Build
 cmake --build cmake-build-"$BUILD_TYPE" -j
---
+++ cmake/FetchVcpkg.cmake
@@ -0,0 +1,46 @@
+include(FetchContent)
+
+option(CT_FETCH_VCPKG "Should the vcpkg be fetched via FetchContent" ON)
+
+function(ct_fetch_vcpkg)
+  if(PROJECT_NAME)
+    message(FATAL_ERROR "ct_fetch_vcpkg must be run before the first project directive")
+  endif()
+
+  if(NOT CT_FETCH_VCPKG)
+    message(STATUS "Skipped fetching vcpkg because CT_FETCH_VCPKG=OFF")
+    return()
+  endif()
+
+  message(STATUS "Fetching vcpkg...")
+  FetchContent_Declare(
+    vcpkg
+    GIT_REPOSITORY https://github.com/microsoft/vcpkg/
+    GIT_TAG 2025.02.14
+    GIT_SHALLOW TRUE
+    GIT_PROGRESS TRUE
+  )
+  FetchContent_MakeAvailable(vcpkg)
+
+  set(VCPKG_BOOTSTRAP_OPTIONS "-disableMetrics" CACHE STRING "")
+
+  if(CMAKE_TOOLCHAIN_FILE)
+    set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_TOOLCHAIN_FILE}" CACHE FILEPATH "")
+  endif()
+  set(CMAKE_TOOLCHAIN_FILE "${vcpkg_SOURCE_DIR}/scripts/buildsystems/vcpkg.cmake"
+    CACHE FILEPATH "Vcpkg toolchain file" FORCE)
+
+  if(CT_TRIPLET_TEMPLATE)
+    cmake_path(GET CT_TRIPLET_TEMPLATE STEM CT_TRIPLET_NAME)
+
+    configure_file(
+      "${CT_TRIPLET_TEMPLATE}"
+      "${CMAKE_BINARY_DIR}/vcpkg-triplets/${CT_TRIPLET_NAME}.cmake"
+      @ONLY
+    )
+    message(STATUS "Generated a vcpkg triplet '${CT_TRIPLET_NAME}' from template")
+
+    set(VCPKG_TARGET_TRIPLET "${CT_TRIPLET_NAME}" CACHE FILEPATH "")
+    set(VCPKG_OVERLAY_TRIPLETS "${CMAKE_BINARY_DIR}/vcpkg-triplets" CACHE FILEPATH "")
+  endif()
+endfunction()
